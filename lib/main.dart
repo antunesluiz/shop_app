2b580651e040b0f5c13343f201086e21d518717e
Luiz Antunes <antunesluiz27@gmail.com>
2021-10-13 01:31:42 -0300

    login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shop_app/routes.dart';
-import 'package:shop_app/screens/splash/splashScreen.dart';
-import 'package:shop_app/themes/constants.dart';
+import 'package:shop_app/screens/splash/SplashScreen.dart';
+import 'package:shop_app/themes/theme.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,15 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        scaffoldBackgroundColor: Colors.white,
-        fontFamily: "Muli",
-        textTheme: TextTheme(
-          bodyText1: TextStyle(color: kTextColor),
-          bodyText2: TextStyle(color: kTextColor),
-        ),
-      ),
+      theme: theme(),
       initialRoute: SplashScreen.routeName,
       routes: routes,
     );
